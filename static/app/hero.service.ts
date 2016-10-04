1fcc71e20386d2462648db1fc3ba86f415dad2c8
alexei-alexov <olexii.lykianchyk@gmail.com>
2016-10-04 16:53:04 +0300

    Init

---
+++ static/app/hero.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import { Headers, Http } from '@angular/http';
+
+import 'rxjs/add/operator/toPromise';
+
+import { Hero } from './hero';
+
+
+@Injectable()
+export class HeroService {
+
+	private heroesUrl = 'app/heroes';
+
+	constructor(private http: Http) { }
+
+	getHeroes(): Promise<Hero[]> {
+		return	this.http.get(this.heroesUrl)
+					.toPromise()
+					.then(response => response.json().data as Hero[])
+					.catch(this.handleError);
+	}
+	
+	private handleError(error: any): Promise<any> {
+		console.error('An error occurred', error);
+		return {Promise.reject(error.message || error);
+	}
+
+	getHeroesSlowly(): Promise<Hero[]> {
+		return new Promise<Hero[]>(resolve =>
+			setTimeout(resolve, 2000)).then(() => this.getHeroes());
+	}
+
+	getHero(id: number): Promise<Hero> {
+		return this.getHeroes().then(heroes => heroes.find(hero => hero.id === id));
+	}
+}
